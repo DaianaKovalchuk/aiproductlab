92ddccbf1245fa408995fcbe5cb889010cbe6ccf
DaianaKovalchuk <dayanakov491@gmail.com>
2026-03-10 23:08:17 +0300

    Update app.py 2

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import numpy as np
-from dotenv import load_dotenv  # <-- исправлено!
-# from pdf_report import build_pdf_bytes  # пока закомментируйте
+from dotenv import load_dotenv
+
+# Раскомментируем первый модуль для проверки
+from pdf_report import build_pdf_bytes
 # from semantic_analyzer import analyze_semantic_consistency, SentenceSc
 # from fact_checker import fact_check_sentences, FactCheckResult
 
 st.title("Тестовый запуск с импортами")
-st.write("Проверяем, работают ли импорты")
+st.write("Проверяем импорт pdf_report")
