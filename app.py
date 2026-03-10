7440ea777063664e8ab35f0ac10b73fa949aab45
DaianaKovalchuk <dayanakov491@gmail.com>
2026-03-10 23:06:09 +0300

    Update app.py 1

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
+import numpy as np
+from dotenv import load_dotenv  # <-- исправлено!
+# from pdf_report import build_pdf_bytes  # пока закомментируйте
+# from semantic_analyzer import analyze_semantic_consistency, SentenceSc
+# from fact_checker import fact_check_sentences, FactCheckResult
 
-st.title("Тестовый запуск")
-st.write("Если вы это видите, проблема была в загрузке моделей или начальном коде.")
-
-# Вся ваша старая логика пока закомментирована
-# model = SentenceTransformer('some-model')  # <-- закомментировано
-if __name__ == "__main__":
-    main()
-
-
+st.title("Тестовый запуск с импортами")
+st.write("Проверяем, работают ли импорты")
