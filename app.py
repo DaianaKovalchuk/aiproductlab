f722dbae816bd2e10d52d0eaa803f8ea12a5fdc2
DaianaKovalchuk <dayanakov491@gmail.com>
2026-03-10 23:17:45 +0300

    Update app.py 5

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import numpy as np
 from dotenv import load_dotenv
 
 from pdf_report import build_pdf_bytes
-from semantic_analyzer import analyze_semantic_consistency  # убрали SentenceSc
-# from fact_checker import fact_check_sentences, FactCheckResult
+from semantic_analyzer import analyze_semantic_consistency
+from fact_checker import fact_check_sentences, FactCheckResult
 
 st.title("Тестовый запуск с импортами")
-st.write("Проверяем импорт analyze_semantic_consistency")
+st.write("Проверяем импорт fact_checker")
